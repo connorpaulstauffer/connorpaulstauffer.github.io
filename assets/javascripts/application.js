b620107b47703e2b9082da01b73e9a3e0a32062f
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-08-27 18:22:09 -0700

    support small screen sizes

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,8 +2,6 @@ $(function () {
   $("#content-grid").isotope({ filter: ".nill" });
 
   var $grid = $("#content-grid").imagesLoaded(function () {
-    // setMasonrySize();
-
     $grid.isotope({
       itemSelector: ".grid-item",
       percentPosition: true,
@@ -20,25 +18,4 @@ $(function () {
   $("#information-button").on("click", function () {
     $("#content-grid").isotope({ filter: ".information" });
   });
-
-  // $( window ).on("resize", setMasonrySize);
-
-  // function setMasonrySize() {
-  //   var windowWidth = $( window ).width();
-  //
-  //   if (windowWidth > 1100) {
-  //     $(".grid-sizer").css("width", "33.33%");
-  //     $(".grid-item").css("width", "33.33%");
-  //   } else if (windowWidth > 700) {
-  //     $(".grid-sizer").css("width", "50%");
-  //     $(".grid-item").css("width", "50%");
-  //   } else {
-  //     $(".grid-sizer").css("width", "100%");
-  //     $(".grid-item").css("width", "100%");
-  //   }
-  //
-  //   $("#content-grid").isotope({
-  //     masonry: { columnWidth: ".grid-sizer" }
-  //   });
-  // };
 });
